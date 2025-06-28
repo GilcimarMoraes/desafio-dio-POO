01010f481f29617d9765f8b2deb3ea074b56db40
GilcimarMoraes <myrraws@yahoo.com.br>
2025-06-28 01:56:20 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -6,6 +8,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
 
         curso1.setTitulo("java");
         curso1.setDescricao("descricao curso java");
@@ -21,5 +24,36 @@ public class Main {
         mentoria1.setData(LocalDate.now());
 
         System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Developer");
+        bootcamp.setDescricao("descricao bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devGil = new Dev();
+        devGil.setNome("Gil");
+        devGil.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Gil: " +  devGil.getConteudosInscritos());
+
+        System.out.println("Conteúdos Concluidos Gil: " +  devGil.getConteudosConcluidos());
+
+        devGil.progredir();
+        System.out.println("------------");
+        System.out.println("Conteúdos Inscritos Gil: " +  devGil.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Gil: " +  devGil.getConteudosConcluidos());
+
+        Dev devGabriel = new Dev();
+        devGabriel.setNome("Gabriel");
+        devGabriel.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Gabriel: " + devGabriel.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Gabriel: " + devGabriel.getConteudosConcluidos());
+        System.out.println("Xp: " + devGil.calcularTotalXp());
+
+        devGabriel.progredir();
+        System.out.println("-----------------");
+        System.out.println("Conteúdos Inscritos Gabriel: " + devGabriel.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Gabriel: " + devGabriel.getConteudosConcluidos());
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,27 +1,10 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -32,15 +15,16 @@ public class Curso {
         this.cargaHoraria = cargaHoraria;
     }
 
-    public int calcuarXP(){
-        return this.cargaHoraria;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
